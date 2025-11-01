d394b3dbf50e3d6d16d3096d35202c17245a1bbe
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-11-01 23:05:15 +0800

    Update KeyboardHandlerM.java

--- src/main/java/net/vulkanmod/mixin/debug/KeyboardHandlerM.java
+++ src/main/java/net/vulkanmod/mixin/debug/KeyboardHandlerM.java
@@ -3,7 +3,6 @@ package net.vulkanmod.mixin.debug;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyboardHandler;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.input.KeyEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,10 +16,14 @@ public abstract class KeyboardHandlerM {
     private boolean handledDebugKey;
 
     @Shadow
-    protected abstract boolean handleChunkDebugKeys(KeyEvent keyEvent);
+    protected abstract boolean handleChunkDebugKeys(net.minecraft.class_11908 keyEvent);
 
-    @Inject(method = "keyPress", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/InputConstants;isKeyDown(Lcom/mojang/blaze3d/platform/Window;I)Z", ordinal = 0, shift = At.Shift.AFTER), remap = false)
-    private void chunkDebug(long window, int action, KeyEvent keyEvent, CallbackInfo ci) {
+    @Inject(
+        method = "method_1466(JILnet/minecraft/class_11908;)V",
+        at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/InputConstants;isKeyDown(Lcom/mojang/blaze3d/platform/Window;I)Z", ordinal = 0, shift = At.Shift.AFTER),
+        remap = false
+    )
+    private void chunkDebug(long window, int key, net.minecraft.class_11908 keyEvent, CallbackInfo ci) {
         // GLFW key 296 -> F7
         // U -> Capture frustum
         this.handledDebugKey |= InputConstants.isKeyDown(Minecraft.getInstance().getWindow(), 296)
